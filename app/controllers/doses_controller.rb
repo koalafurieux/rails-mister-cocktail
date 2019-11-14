730fee0d8318406c684f55b9f68da8b0b688ea16
Simon Souillart <souillartdruez@gmail.com>
2019-11-14 16:52:54 +0100

    added render new if unsaved new cocktail

--- app/controllers/doses_controller.rb
+++ app/controllers/doses_controller.rb
@@ -8,12 +8,17 @@ class DosesController < ApplicationController
     @dose = Dose.new(dose_params)
     @cocktail = Cocktail.find(params[:cocktail_id])
     @dose.cocktail = @cocktail
-    @dose.save
-    redirect_to cocktail_path(@cocktail)
+    if @dose.save
+      redirect_to cocktail_path(@cocktail)
+    else
+      render :new
+    end
   end
 
   def destroy
-    @dose = Dose.find(params[:cocktail_id])
+    @dose = Dose.find(params[:id])
+    @dose.destroy
+    redirect_to cocktail_path(@dose.cocktail)
   end
 
   private
