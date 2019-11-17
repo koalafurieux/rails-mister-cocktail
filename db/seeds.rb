01b2df50864eb6b652a1f97e4be4560a6426d2a9
Simon Souillart <souillartdruez@gmail.com>
2019-11-17 13:03:29 +0100

    style : banners and containers

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,6 +8,13 @@
 require 'json'
 require 'open-uri'
 Cocktail.destroy_all
+
+puts "Creating Cocktails"
+cocktails = ['Tiger', 'Marcel', 'Gin Tonic', 'Vodka Lemon']
+cocktails.each do |cocktail|
+  c = Cocktail.create(name: cocktail)
+end
+
 Ingredient.destroy_all
 result = JSON.parse(open('http://www.thecocktaildb.com/api/json/v1/1/list.php?i=list').read)
 drinks = result['drinks']
